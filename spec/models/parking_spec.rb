8a084a8833c6752fbf5f98a710caf1c60c66cd4b
rebecca9201 <wangmmqq@gmail.com>
2017-06-07 16:59:07 +0800

    增加长期计费

--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Parking, type: :model do
    end
  end
 
+
  describe ".calculate_amount" do
 
    before do
@@ -94,6 +95,45 @@ RSpec.describe Parking, type: :model do
       @parking.calculate_amount
        expect(@parking.amount).to eq(300)
      end
+   end
+
+
+   context "long-term" do
+     before do
+       @user = User.create( :email => "test@example.com", :password => "123455678")
+       @parking = Parking.new( :parking_type => "long-term", :user => @user, :start_at => @time )
+     end
+
+     it "6 hours should be ¥12" do
+       @parking.end_at = @time + 6.hours
+       @parking.calculate_amount
+       expect(@parking.amount).to eq(1200)
+     end
+
+     it "24 hours should be ¥16" do
+       @parking.end_at = @time + 12.hours
+       @parking.calculate_amount
+       expect(@parking.amount).to eq(1600)
+     end
+
+     it "25 hours should be ¥32" do
+       @parking.end_at = @time + 25.hours
+       @parking.calculate_amount
+       expect(@parking.amount).to eq(3200)
+     end
+
+     it "48 hours should be ¥32" do
+       @parking.end_at = @time + 48.hours
+       @parking.calculate_amount
+       expect(@parking.amount).to eq(3200)
+     end
+
+     it "50 hours should be ¥48 " do
+       @parking.end_at = @time + 50.hours
+       @parking.calculate_amount
+       expect(@parking.amount).to eq(4800)
+     end
+
    end
  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
